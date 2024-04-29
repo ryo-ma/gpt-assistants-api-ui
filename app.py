d4155b9412a4036785585a8f04d31adfdf951bf0
ryo-ma <saka_ro@yahoo.co.jp>
2024-04-30 00:37:16 +0900

    Support for assistants api v2

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
-import time
 import base64
 import re
 import json
 
 import streamlit as st
 import openai
-from openai.types.beta.threads import MessageContentImageFile
+from openai import AssistantEventHandler
 from tools import TOOL_MAP
+from typing_extensions import override
 
 
 azure_openai_endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT")
@@ -30,59 +30,29 @@ enabled_file_upload_message = os.environ.get(
 )
 
 
-def create_thread(content, file):
-    messages = [
-        {
-            "role": "user",
-            "content": content,
-        }
-    ]
-    if file is not None:
-        messages[0].update({"file_ids": [file.id]})
-    thread = client.beta.threads.create(messages=messages)
-    return thread
+class EventHandler(AssistantEventHandler):
+    @override
+    def on_event(self, event):
+        pass
 
+    @override
+    def on_text_created(self, text):
+        st.session_state.current_message = ""
+        with st.chat_message("Assistant"):
+            st.session_state.current_markdown = st.empty()
 
-def create_message(thread, content, file):
-    file_ids = []
-    if file is not None:
-        file_ids.append(file.id)
-    client.beta.threads.messages.create(
-        thread_id=thread.id, role="user", content=content, file_ids=file_ids
-    )
+    @override
+    def on_text_delta(self, delta, snapshot):
+        if delta.value:
+            st.session_state.current_message += delta.value
+            st.session_state.current_markdown.markdown(st.session_state.current_message, True)
 
-
-def create_run(thread):
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id, assistant_id=assistant_id, instructions=instructions
-    )
-    return run
-
-
-def create_file_link(file_name, file_id):
-    content = client.files.content(file_id)
-    content_type = content.response.headers["content-type"]
-    b64 = base64.b64encode(content.text.encode(content.encoding)).decode()
-    link_tag = f'<a href="data:{content_type};base64,{b64}" download="{file_name}">Download Link</a>'
-    return link_tag
-
-
-def get_message_value_list(messages):
-    messages_value_list = []
-    for message in messages:
-        message_content = ""
-        print(message)
-        if not isinstance(message, MessageContentImageFile):
-            message_content = message.content[0].text
-            annotations = message_content.annotations
-        else:
-            image_file = client.files.retrieve(message.file_id)
-            messages_value_list.append(
-                f"Click <here> to download {image_file.filename}"
-            )
+    @override
+    def on_text_done(self, text):
         citations = []
-        for index, annotation in enumerate(annotations):
-            message_content.value = message_content.value.replace(
+        text_value = text.value
+        for index, annotation in enumerate(text.annotations):
+            text_value = text.value.replace(
                 annotation.text, f" [{index}]"
             )
 
@@ -93,113 +63,117 @@ def get_message_value_list(messages):
                 )
             elif file_path := getattr(annotation, "file_path", None):
                 link_tag = create_file_link(
-                    annotation.text.split("/")[-1], file_path.file_id
+                    annotation.text.split("/")[-1], file_path.file_id,
                 )
-                message_content.value = re.sub(
-                    r"\[(.*?)\]\s*\(\s*(.*?)\s*\)", link_tag, message_content.value
+                text_value = re.sub(
+                    r"\[(.*?)\]\s*\(\s*(.*?)\s*\)", link_tag, text_value
                 )
+        st.session_state.current_markdown.markdown(text_value, True)
+        st.session_state.chat_log.append({"name": "assistant", "msg": text_value})
 
-        message_content.value += "\n" + "\n".join(citations)
-        messages_value_list.append(message_content.value)
-        return messages_value_list
+    @override
+    def on_tool_call_created(self, tool_call):
+        if tool_call.type == 'code_interpreter':
+            st.session_state.current_tool_input = ""
+            with st.chat_message("Assistant"):
+                st.session_state.current_tool_input_markdown = st.empty()
+
+    @override
+    def on_tool_call_delta(self, delta, snapshot):
+        if st.session_state.current_tool_input_markdown is None:
+            with st.chat_message("Assistant"):
+                st.session_state.current_tool_input_markdown = st.empty()
+
+        if delta.type == 'code_interpreter':
+            if delta.code_interpreter.input:
+                st.session_state.current_tool_input += delta.code_interpreter.input
+                input_code = f"### code interpreter\ninput:\n```python\n{st.session_state.current_tool_input}\n```"
+                st.session_state.current_tool_input_markdown.markdown(input_code, True)
+
+            if delta.code_interpreter.outputs:
+                for output in delta.code_interpreter.outputs:
+                    if output.type == "logs":
+                        pass
+
+    @override
+    def on_tool_call_done(self, tool_call):
+
+        st.session_state.tool_calls.append(tool_call)
+        if tool_call.type == "code_interpreter":
+            if tool_call.id in [x.id for x in st.session_state.tool_calls]:
+                return
+            input_code = f"### code interpreter\ninput:\n```python\n{tool_call.code_interpreter.input}\n```"
+            st.session_state.current_tool_input_markdown.markdown(input_code, True)
+            st.session_state.chat_log.append({"name": "assistant", "msg": input_code})
+            st.session_state.current_tool_input_markdown = None
+            for output in tool_call.code_interpreter.outputs:
+                if output.type == "logs":
+                    output = f"### code interpreter\noutput:\n```\n{output.logs}\n```"
+                    with st.chat_message("Assistant"):
+                        st.markdown(output, True)
+                        st.session_state.chat_log.append({"name": "assistant", "msg": output})
+        elif tool_call.type == 'function' and self.current_run.status == 'requires_action':
+            with st.chat_message("Assistant"):
+                msg = f"###Function Calling: {tool_call.function.name}"
+                st.markdown(f"###Function Calling: {tool_call.function.name}", True)
+                st.session_state.chat_log.append({"name": "assistant", "msg": msg})
+            tool_calls = self.current_run.required_action.submit_tool_outputs.tool_calls
+            tool_outputs = []
+            for submit_tool_call in tool_calls:
+                tool_function_name = submit_tool_call.function.name
+                tool_function_arguments = json.loads(submit_tool_call.function.arguments)
+                tool_function_output = TOOL_MAP[tool_function_name](**tool_function_arguments)
+                tool_outputs.append({"tool_call_id": submit_tool_call.id, "output": tool_function_output})
+
+            with client.beta.threads.runs.submit_tool_outputs_stream(
+                thread_id=st.session_state.thread.id,
+                run_id=self.current_run.id,
+                tool_outputs=tool_outputs,
+                event_handler=EventHandler(),
+            ) as stream:
+                stream.until_done()
 
 
-def get_message_list(thread, run):
-    completed = False
-    while not completed:
-        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-        print("run.status:", run.status)
-        messages = client.beta.threads.messages.list(thread_id=thread.id)
-        print("messages:", "\n".join(get_message_value_list(messages)))
-        if run.status == "completed":
-            completed = True
-        elif run.status == "failed":
-            break
-        else:
-            time.sleep(1)
-
-    messages = client.beta.threads.messages.list(thread_id=thread.id)
-    return get_message_value_list(messages)
+def create_thread(content, file):
+    messages = [
+        {
+            "role": "user",
+            "content": content,
+        }
+    ]
+    if file is not None:
+        messages[0].update({"file_ids": [file.id]})
+    thread = client.beta.threads.create()
+    return thread
 
 
-def get_response(user_input, file):
+def create_message(thread, content, file):
+    attachments = []
+    if file is not None:
+        attachments.append({"file_id": file.id, "tools": [{"type": "code_interpreter"}]})
+    client.beta.threads.messages.create(
+        thread_id=thread.id, role="user", content=content, attachments=attachments
+    )
+
+
+def create_file_link(file_name, file_id):
+    content = client.files.content(file_id)
+    content_type = content.response.headers["content-type"]
+    b64 = base64.b64encode(content.text.encode(content.encoding)).decode()
+    link_tag = f'<a href="data:{content_type};base64,{b64}" download="{file_name}">Download Link</a>'
+    return link_tag
+
+
+def run_stream(user_input, file):
     if "thread" not in st.session_state:
         st.session_state.thread = create_thread(user_input, file)
-    else:
-        create_message(st.session_state.thread, user_input, file)
-    run = create_run(st.session_state.thread)
-    run = client.beta.threads.runs.retrieve(
-        thread_id=st.session_state.thread.id, run_id=run.id
-    )
-
-    while run.status == "in_progress":
-        print("run.status:", run.status)
-
-        time.sleep(1)
-        run = client.beta.threads.runs.retrieve(
-            thread_id=st.session_state.thread.id, run_id=run.id
-        )
-        run_steps = client.beta.threads.runs.steps.list(
-            thread_id=st.session_state.thread.id, run_id=run.id
-        )
-        print("run_steps:", run_steps)
-        for step in run_steps.data:
-            if hasattr(step.step_details, "tool_calls"):
-                for tool_call in step.step_details.tool_calls:
-                    if (
-                        hasattr(tool_call, "code_interpreter")
-                        and tool_call.code_interpreter.input != ""
-                    ):
-                        input_code = f"### code interpreter\ninput:\n```python\n{tool_call.code_interpreter.input}\n```"
-                        print(input_code)
-                        if len(
-                            st.session_state.tool_calls
-                        ) == 0 or tool_call.id not in [
-                            x.id for x in st.session_state.tool_calls
-                        ]:
-                            st.session_state.tool_calls.append(tool_call)
-                            with st.chat_message("Assistant"):
-                                st.markdown(
-                                    input_code,
-                                    True,
-                                )
-                                st.session_state.chat_log.append(
-                                    {
-                                        "name": "assistant",
-                                        "msg": input_code,
-                                    }
-                                )
-
-    if run.status == "requires_action":
-        print("run.status:", run.status)
-        run = execute_action(run, st.session_state.thread)
-
-    return "\n".join(get_message_list(st.session_state.thread, run))
-
-
-def execute_action(run, thread):
-    tool_outputs = []
-    for tool_call in run.required_action.submit_tool_outputs.tool_calls:
-        tool_id = tool_call.id
-        tool_function_name = tool_call.function.name
-        print(tool_call.function.arguments)
-
-        tool_function_arguments = json.loads(tool_call.function.arguments)
-
-        print("id:", tool_id)
-        print("name:", tool_function_name)
-        print("arguments:", tool_function_arguments)
-
-        tool_function_output = TOOL_MAP[tool_function_name](**tool_function_arguments)
-        print("tool_function_output", tool_function_output)
-        tool_outputs.append({"tool_call_id": tool_id, "output": tool_function_output})
-
-    run = client.beta.threads.runs.submit_tool_outputs(
-        thread_id=thread.id,
-        run_id=run.id,
-        tool_outputs=tool_outputs,
-    )
-    return run
+    create_message(st.session_state.thread, user_input, file)
+    with client.beta.threads.runs.stream(
+        thread_id=st.session_state.thread.id,
+        assistant_id=assistant_id,
+        event_handler=EventHandler(),
+    ) as stream:
+        stream.until_done()
 
 
 def handle_uploaded_file(uploaded_file):
@@ -259,12 +233,7 @@ def main():
         file = None
         if uploaded_file is not None:
             file = handle_uploaded_file(uploaded_file)
-        with st.spinner("Wait for response..."):
-            response = get_response(user_msg, file)
-        with st.chat_message("Assistant"):
-            st.markdown(response, True)
-
-        st.session_state.chat_log.append({"name": "assistant", "msg": response})
+        run_stream(user_msg, file)
         st.session_state.in_progress = False
         st.session_state.tool_call = None
         st.rerun()
